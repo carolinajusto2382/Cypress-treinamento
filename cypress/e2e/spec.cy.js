0fc0a266ed6fd82f1c62edc623f915dc348c6776
carolinajusto2382 <caroljusto64@gmail.com>
2024-07-15 21:51:56 -0300

    Teste com cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe('Visitando a página', () => {
+  it('Acessando a url', () => {
+    cy.visit('https://example.cypress.io')
+  })
+}),
+
+describe('Acessando utilities', () => {
+  it('Clicando em utilities', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Utilities').click();
+
+    cy.url().should('include', '/utilities')
+  })
+}),
+
+describe('Acessando assertions', () => {
+  it('Clicando em commands', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Commands').click();
+    cy.contains('Assertions').click();
+
+    cy.url().should('include', '/commands/assertions')
+  })
+})
