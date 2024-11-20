db244c42db436007099a172051adb5cc23df5800
carolinajusto2382 <caroljusto64@gmail.com>
2024-11-20 16:14:11 -0300

    Cenários atualizados

--- cypress/e2e/buggy.cy.js
+++ cypress/e2e/buggy.cy.js
@@ -9,17 +9,17 @@ context ('Realizando testes no site Buggy Rating', () => {
 
     describe('Home', () => {
 
-        it.skip('Deve ser possível clicar em carro Popular Make', () => {
+        it('Deve ser possível clicar em carro Popular Make', () => {
             cy.get('[title="Lamborghini"]').click();
             cy.contains('Model').should('be.visible');
 
         })
-        it.skip('Deve ser possível clicar em Popular Model', () => {
+        it('Deve ser possível clicar em Popular Model', () => {
             cy.get('[title="Diablo"]').click();
             cy.contains('Specification').should('be.visible');
 
         })
-        it.skip('Deve ser possível clicar em Overall Rating', () => {
+        it('Deve ser possível clicar em Overall Rating', () => {
             cy.get(':nth-child(3) > .card > a > .img-fluid').click();
             cy.contains('Make').should('be.visible');
 
@@ -43,10 +43,69 @@ context ('Realizando testes no site Buggy Rating', () => {
 
         })
 
+        describe('Campos obrigatórios e validações', () => {
+
+            it('Não deve ser possível registrar com o campo Login vazio', () => {
+                cy.get('.btn-success-outline').click();
+
+                cy.get('#firstName').type('Carolina');
+                cy.get('#lastName').type('Justo');
+                cy.get('#password').type('969696Ca@');
+                cy.get('#confirmPassword').type('969696Ca@');
+
+                cy.get('.btn-default').should('be.disabled');
+            })
+
+            it('Não deve ser possível registrar com o campo First name vazio', () => {
+                cy.get('.btn-success-outline').click();
+
+                cy.get('#username').type(geraNickname());
+                cy.get('#lastName').type('Justo');
+                cy.get('#password').type('969696Ca@');
+                cy.get('#confirmPassword').type('969696Ca@');
+
+                cy.get('.btn-default').should('be.disabled');
+            })
+
+            it('Não deve ser possível registrar com o campo Last name vazio', () => {
+                cy.get('.btn-success-outline').click();
+
+                cy.get('#username').type(geraNickname());
+                cy.get('#firstName').type('Débora');
+                cy.get('#password').type('969696Ca@');
+                cy.get('#confirmPassword').type('969696Ca@');
+
+                cy.get('.btn-default').should('be.disabled');
+            })
+
+            it('Não deve ser possível registrar com o campo Password vazio', () => {
+                cy.get('.btn-success-outline').click();
+
+                cy.get('#username').type(geraNickname());
+                cy.get('#firstName').type('Débora');
+                cy.get('#lastName').type('Justo');
+                cy.get('#confirmPassword').type('969696Ca@');
+
+                cy.get('.btn-default').should('be.disabled');
+            })
+
+            it('Não deve ser possível registrar com o campo Confirm password vazio', () => {
+                cy.get('.btn-success-outline').click();
+
+                cy.get('#username').type(geraNickname());
+                cy.get('#firstName').type('Débora');
+                cy.get('#lastName').type('Justo');
+                cy.get('#password').type('969696Ca@');
+
+                cy.get('.btn-default').should('be.disabled');
+            })
+            
+        })
+
     })
 
     describe('Login', () => {
-        
+
         it('Deve ser possível logar com credenciais válidas', () => {
             cy.get('.input-sm').type('CarolJusto2');
             cy.get('[name="password"]').type('30117556Ca@');
